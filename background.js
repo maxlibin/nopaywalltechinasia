10a9d5b0294dbba64846a5dd1edbcc4cae3a230d
maxlibin <me@maxlibin.com>
2021-06-18 23:09:57 +0800

    use simple css hack

--- background.js
+++ background.js
@@ -1,6 +1,10 @@
-let color = '#3aa757';
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  // read changeInfo data and do something with it
+  // like send the new url to contentscripts.js
+  if (changeInfo.url) {
+    chrome.tabs.sendMessage(tabId, {
+      message: "TabUrlUpdated",
+      url: changeInfo.url,
+    });
+  }
 });
